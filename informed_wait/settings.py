da4dd01eb3bfcc9663baf2553daf8f4a7fa874a1
Bora Oztekin <boramoztekin@gmail.com>
2018-11-17 12:27:11 -0600

    Add simplified staticfiles package

--- informed_wait/settings.py
+++ informed_wait/settings.py
@@ -26,6 +26,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['informed-wait.herokuapp.com']
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Application definition
 
@@ -46,6 +50,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'informed_wait.urls'
