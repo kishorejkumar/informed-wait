c7bd5635a339e64691e53bcca65c4a93bc180609
Bora Oztekin <boramoztekin@gmail.com>
2018-11-17 13:07:20 -0600

    Set restaurant model as managed

--- informed_wait/models.py
+++ informed_wait/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Restaurant(models.Model):
     name = models.CharField(max_length=64)
     wait_time = models.CharField(max_length=8)
+
+    class Meta:
+        managed = True
