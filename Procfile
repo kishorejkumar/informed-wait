be2d648dc0ea2b9b0a7d6d2dece08c81e8960915
Bora Oztekin <boramoztekin@gmail.com>
2018-11-17 09:14:53 -0600

    Add Procfile and database connection
    Add accompanying requirements.txt

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn informed_wait.wsgi log.txt
--- informed_wait/settings.py
+++ informed_wait/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'informed_wait.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('PGSQL_NAME'),
+        'USER': os.environ.get('PGSQL_USER'),
+        'PASSWORD': os.environ.get('PGSQL_PASSPHRASE'),
+        'HOST': os.environ.get('PGSQL_HOST'),
+        'PORT': '5432'
     }
 }
 
